0c019172872336285c755353bb305dc96ab88a26
pyMatJ <62421390+pyMatJ@users.noreply.github.com>
2022-11-21 20:18:08 +0100

    Preparing for release v0.0.0

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
    name='DigiclearJSONConverter',
-   version='0.0',
+   version='0.0.0',
    description='Converts JSON history files from digiclear',
    url='',
-   author='Mathieu Jeannin, Paul Goulain',
+   authors='Mathieu Jeannin, Paul Goulain',
    packages=find_packages(),
    install_requires=['PyQt5', 'reportlab', 'requests', 'lxml'],
    entry_points={'console_scripts': ['DigiclearJSONConverter = DigiclearJSONConverter.gui.GUIConverter:main']},
